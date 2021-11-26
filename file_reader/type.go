30646e98f7c28c3b6f92471c1792f8811b51a07f
Yurino Oga <yurino.o@latona.io>
2021-11-26 16:21:40 +0900

    release

--- file_reader/type.go
+++ file_reader/type.go
@@ -42,7 +42,7 @@ type EC_MC struct {
 		} `json:"work"`
 	} `json:"production_order"`
 	APISchema               string `json:"api_schema"`
-	Product                 string `json:"material_code"`
+	MaterialCode            string `json:"material_code"`
 	Plant_Supplier          string `json:"plant/supplier"`
 	Stock                   float64 `json:"stock"`
 	GoodsMovementType       string `json:"document_type"`
